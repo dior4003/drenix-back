3a793fc7bbcdc18dfc8bd6a77a49563ed8aa9c25
dior4003 <diyor3933@gmail.com>
2022-10-26 16:27:56 +0000

    started

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv").config()
+const url =process.env.MongoDB;
+// const mcqRouter = require('./routers/mcq');
+const app = express();
+mongoose.connect( url, { useNewUrlParser: true , useUnifiedTopology:true});
+const con = mongoose.connection;
+
+
+// middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use(express.json()); 
+
+// database connection
+con.on('open', function(){
+    console.log('connected')
+});
+// app.use('/mcq',mcqRouter);
+
+
+// start
+app.listen(process.env.PORT, function(){
+    console.log('server worked');
+});
