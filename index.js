e12bf4f3a3bd7381fdd1a3da4011ffa6a1b2d3ef
dior4003 <diyor3933@gmail.com>
2022-10-29 01:57:21 -0400

    tt

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const app = express();
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const con = mongoose.connection;
 
-const uzb = require("./public/main");
-const ru = require("./public/main");
+const { uzb, ru } = require("./public/main");
 // console.log(uzb);
 // middleware
 app.use(express.urlencoded({ extended: true }));
